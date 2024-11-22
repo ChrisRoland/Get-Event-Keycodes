f0e416b73686505ec77a0cb679afb1171642be2f
ChrisRoland <chrisebuberoland@gmail.com>
2024-11-22 14:39:00 +0100

    feat:complete

---
+++ script.js
@@ -0,0 +1,29 @@
+//Jquery
+const insert = $("#insert");
+
+$(window).keydown((e) => {
+  const keyDisplay = `
+      <div class='key'>
+        ${
+        e.key === " " ? "Space" : e.key
+      }
+      <small>event.key</small></div>
+      <div class='key'>${e.keyCode}<small>event.keyCode</small></div>
+      <div class='key'>${e.code}<small>event.code</small></div>
+    `;
+
+  insert.html(keyDisplay);
+
+});
+
+//Vanilla Js
+// const insert = document.getElementById("insert");
+
+// window.addEventListener("keydown", (e) => {
+//   insert.innerHTML = `<div class='key'>${
+//     e.key === " " ? "Space" : e.key
+//   }<small>event.key</small></div>
+//       <div class='key'>${e.keyCode}<small>event.keyCode</small></div>
+//       <div class='key'>${e.code}<small>event.code</small></div>
+//     `;
+// });
